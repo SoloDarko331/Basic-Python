b81936a6aba8c839a1ca7e6bfded5407d6763c29
Onyxx <44676732+SoloDarko331@users.noreply.github.com>
2021-10-22 09:58:42 -0400

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,10 @@
+self_name = "Brandon Halls"
+self_age = "28"
+self_city = "Brooklyn, NY"
+self_hobby = "playing video games"
+
+print("Hello World!")
+print("My name is " + self_name + ". ")
+print("I am " + self_age + " years old. ")
+print("I currently reside in " + self_city + ". ")
+print("My favorite hobby is " + self_hobby + ". ")
