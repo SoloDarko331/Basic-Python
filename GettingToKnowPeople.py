07e6fbabad3a156b202d0a3597c088e976c8ce27
Onyxx <44676732+SoloDarko331@users.noreply.github.com>
2021-12-15 21:06:20 -0500

    Add files via upload

---
+++ GettingToKnowPeople.py
@@ -0,0 +1,16 @@
+#Getting To Know People
+print("Hello. My name is Brandon.")
+print("\nI created this small program to get to know people better.")
+
+name = input("\nWhat is your name? ")
+print("\nHello " + name)
+
+current_year = input("\nWhat year are we in? " )
+birth_year = input("\nWhat year were you born? ")
+age = int(current_year) - int(birth_year)
+print("\nSo by my calculation, that would make you ", + age , " years old.")
+print("\nThat's not a bad age. Not bad at all.")
+
+fav_hobby = input("\nSo, what's your favorite hobby? ")
+least_hobby = input("\nAlright that sounds cool. What about your least favorite hobby? ")
+print("\nThat's what's up. My favorite hobby is playing video games and my least favorite is reading books.\n")
